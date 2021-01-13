c0e57be148158bd8f9a029958dbdfd39a6817978
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-01-13 14:57:02 -0500

    follower displaying

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -7,16 +7,17 @@ class App extends React.Component {
   state = {
     usersImg: [],
     name: "",
-    followers:[],
   }
 
   componentDidMount() {
-    axios.get("https://api.github.com/users/dustinmyers")
+    axios
+    .get("https://api.github.com/users/dustinmyers")
     .then((res) => {
       console.log(res.data)
       this.setState({
         usersImg: res.data.avatar_url,
         name: res.data.name,
+        
       })
     })
     .catch((err) => {
@@ -31,7 +32,7 @@ class App extends React.Component {
       <div className="App">
         <img src={this.state.usersImg}></img>
         <h2>{this.state.name}</h2>
-        <Card followers={this.state.followers}/>
+        <Card />
       </div>
     );
   }
--- user-card/src/components/card.js
+++ user-card/src/components/card.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React ,{ useEffect }from "react";
 import axios from "axios";
+
+
+
+// const Card = (props) => {
+
+//     useEffect(() => {
+//         axios
+//         .get("https://api.github.com/users/dustinmyers/followers")
+//         .then((res) => {
+//             console.log(res)
+
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+//     },[]);
+
+//     return(
+//         <div>
+
+//         </div>
+//     )
+// }
+
+// export default Card;
+
 
 class Card extends React.Component {
 
+  state = {
+    followers: []
+  }
+
     componentDidMount() {
-        axios.get("https://api.github.com/users/dustinmyers/followers")
+        axios
+        .get("https://api.github.com/users/dustinmyers/followers")
         .then((res) => {
           console.log(res.data)
           this.setState({
+            userImg: res.data.avatar_url,
             followers: res.data
           })
         })
@@ -19,10 +51,10 @@ class Card extends React.Component {
     render() {
         return(
             <div>
-                {this.props.followers.map(follower => (
-                    <div key={followers.length} className="follower">
+                {this.state.followers.map(follower => (
+                    <div key={follower.id} className="follower">
                         <img src={follower.avatar_url}/>
-                        <h2>{follower.name}</h2>
+                        <h2>{follower.login}</h2>
                     </div>
 
                 ))}
