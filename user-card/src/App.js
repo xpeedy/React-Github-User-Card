99908ada260d59acbe9a51cfc8112aaba33bae7c
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-01-12 20:51:55 -0500

    making card

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import axios from "axios";
+import Card from "./components/card"
 
 class App extends React.Component {
   state = {
@@ -23,25 +24,14 @@ class App extends React.Component {
     })
   }
 
-  componentDidUpdate() {
-    axios.get("https://api.github.com/users/dustinmyers/followers")
-    .then((res) => {
-      console.log(res.data)
-      this.setState({
-        followers: res.data
-      })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+  
 
   render(){
     return (
       <div className="App">
         <img src={this.state.usersImg}></img>
         <h2>{this.state.name}</h2>
-        <p>{this.state.data}</p>
+        <Card followers={this.state.followers}/>
       </div>
     );
   }
--- user-card/src/components/card.js
+++ user-card/src/components/card.js
@@ -1,11 +1,34 @@
 import React from "react";
-
+import axios from "axios";
 
 class Card extends React.Component {
 
+    componentDidMount() {
+        axios.get("https://api.github.com/users/dustinmyers/followers")
+        .then((res) => {
+          console.log(res.data)
+          this.setState({
+            followers: res.data
+          })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
+
     render() {
         return(
-            <div></div>
+            <div>
+                {this.props.followers.map(follower => (
+                    <div key={followers.length} className="follower">
+                        <img src={follower.avatar_url}/>
+                        <h2>{follower.name}</h2>
+                    </div>
+
+                ))}
+            </div>
         )
     }
 }
+
+export default Card;
